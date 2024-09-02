1d887db4f72053df2f6e8e6ba271e1b2278a7181
Mikael de Jesus <mikaeljl824@gmail.com>
2024-09-02 20:39:51 -0300

    Adicionar arquivos de projeto.

---
+++ calculador de Salario/Program.cs
@@ -0,0 +1,10 @@
+﻿double Sal, Dollar,horas;
+
+Console.WriteLine("Digite Valor da Moeda:");
+Dollar = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite as horas trabalhadas");
+horas = double.Parse(Console.ReadLine());
+
+Sal = Dollar * horas;
+
+Console.WriteLine("valor do salario:" + Sal);
